15ac5b8b7ab3547219f2d45d07404b1a26506c05
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-18 05:04:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,5 @@
 def reformat_languages(languages)
   new_hash = {}
- languages.each{|key, value| new_hash[key.to_sym] = value.to_s}
+ languages.each{|key, value| new_hash[key.to_sym] = {second_level_key: "second level value"} }
  new_hash
 end
